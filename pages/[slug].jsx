ec0b12c19edf0c1868ecfb778882c3502a415983
Forge-Creative <webmaster@forge.co.nz>
2023-02-17 16:09:12 +1300

    Add conditional for feature image

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 const Post = () => {
-    const { query } = useRouter()
-    const router = useRouter()
-    const { data } = useQuery(gql`
+  const { query } = useRouter()
+  const router = useRouter()
+  const { data } = useQuery(gql`
     query postQuery {
         post(id: "${query.slug}", idType: SLUG) {
             title
@@ -19,23 +19,26 @@ const Post = () => {
         }
         `);
 
-    const post = data?.post
-    const imageUrl = data?.post.featuredImage.node.sourceUrl
+  const post = data?.post
+  const imageUrl = data?.post.featuredImage?.node.sourceUrl
 
-    if (!post) {
-        return <main><div>loading...</div></main>
-    }
-    return (
-        <main>
-            <a href="#" onClick={() => router.back()}>Back</a>
-            <div className='post-content'>
-            
-            <h1>{post.title}</h1>
-            <Image src={imageUrl} alt='feature image' width={400} height={300}/>
-            <h3 dangerouslySetInnerHTML={{ __html: post.content }} />
-            </div>
-        </main>
-    )
+  if (!post) {
+    return <main><div>loading...</div></main>
+  }
+  return (
+    <main>
+      <a href="#" onClick={() => router.back()}>Back</a>
+      <div className='post-content'>
+
+        <h1>{post.title}</h1>
+        {imageUrl && (
+          <div style={{width: '100%', height: '100%', position: 'relative'}}>
+            <Image src={imageUrl} alt='feature image' width={800} height={600} />
+          </div>)}
+        <div class='post-main' dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </main>
+  )
 }
 
 export default Post
